b2e13fc45f7619e105f4ca87d12b9fca573bebbb
jacek010 <jmyjkowski@gmail.com>
2024-01-25 12:28:57 +0100

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Jacek Myjkowski on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
