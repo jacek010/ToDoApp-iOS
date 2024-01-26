71799630fbc1996c3cfc7886cea67419b3e4c749
jacek010 <jmyjkowski@gmail.com>
2024-01-26 13:59:30 +0100

    Profile tab and sign out working

--- ToDoApp/Views/ProfileView.swift
+++ ToDoApp/Views/ProfileView.swift
@@ -15,10 +15,61 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                
+                if let user = viewModel.user{
+                    profile(user: user)
+                } else {
+                    Text("Profile loading...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user:User)->some View{
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // Info: Name, Email, Member since
+        VStack{
+            HStack{
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack{
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack{
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        // Sign out
+        Button("Log Out"){
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        .bold()
+        
+        Spacer()
     }
 }
 
